2f643003cf63b31c68116b9c893faf4a66fd7975
egarcia- <egarcia-@MacBook-Pro-de-egarcia.local>
2020-02-18 22:05:34 +0100

    fixed GNL

--- get_next_line.c
+++ get_next_line.c
@@ -3,16 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: byrd <byrd@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: egarcia- <egarcia-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 18:42:52 by egarcia-          #+#    #+#             */
-/*   Updated: 2019/11/27 11:21:18 by byrd             ###   ########.fr       */
+/*   Updated: 2020/02/18 11:19:05 by egarcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int					ft_putline(int fd, char **s, char **line)
+static void			ft_strdel(char **str)
+{
+	if (str != NULL && *str != NULL)
+	{
+		free(*str);
+		*str = NULL;
+	}
+}
+
+int					ft_putline(int fd, char **s, char **line)
 {
 	int		size;
 	char	*tmp;
@@ -27,14 +36,14 @@ static int					ft_putline(int fd, char **s, char **line)
 	return (1);
 }
 
-static int					output(int ret, int fd, char **s, char **line)
+int					output(int ret, int fd, char **s, char **line)
 {
 	if (ret < 0)
 		return (-1);
 	else if (ret == 0 && (s[fd] == NULL || s[fd][0] == '\0'))
 	{
 		*line = ft_strdup("");
-		free(s);
+		ft_strdel(&s[fd]);
 		return (0);
 	}
 	else if (ft_strchr(s[fd], '\n'))
@@ -42,7 +51,7 @@ static int					output(int ret, int fd, char **s, char **line)
 	else
 	{
 		*line = ft_strdup(s[fd]);
-		free(s);
+		ft_strdel(&s[fd]);
 		return (0);
 	}
 }
@@ -51,12 +60,14 @@ int					get_next_line(int fd, char **line)
 {
 	int				ret;
 	static char		*s[4096];
-	char			buff[512];
+	char			*buff;
 	char			*tmp;
 
 	if (fd < 0 || line == NULL)
 		return (-1);
-	while ((ret = read(fd, buff, 511)) > 0)
+	if (!(buff = (char*)malloc(sizeof(char) * 32 + 1)))
+		return (-1);
+	while ((ret = read(fd, buff, 32)) > 0)
 	{
 		buff[ret] = '\0';
 		if (s[fd] == NULL)
@@ -70,5 +81,6 @@ int					get_next_line(int fd, char **line)
 		if (ft_strchr(s[fd], '\n'))
 			break ;
 	}
+	free(buff);
 	return (output(ret, fd, s, line));
 }
