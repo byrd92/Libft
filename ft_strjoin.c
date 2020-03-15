7e3a4fbac434c106e492b858162523a39d496c6d
egarcia- <egarcia-@MacBook-Pro-de-egarcia.local>
2020-03-15 09:50:52 +0100

    norme fixed

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: egarcia- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: egarcia- <emilioggo@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 16:52:52 by egarcia-          #+#    #+#             */
-/*   Updated: 2019/11/19 19:06:34 by egarcia-         ###   ########.fr       */
+/*   Updated: 2020/03/15 09:50:29 by egarcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = 0;
 	if (s1 && s2)
 	{
-		if (!(str = (char*)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char))))
+		if (!(str = (char*)malloc((ft_strlen(s1) + ft_strlen(s2) + 1)
+			* sizeof(char))))
 			return (NULL);
 		while (*s1)
 			str[i++] = *s1++;
